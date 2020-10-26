992bb542e171e0e6ce13b31498e7151e11eb99c6
招雄彬 <614619114@qq.com>
2020-10-26 11:49:45 +0800

    尝试使用mysql2链接（失败）

--- blog25/lib/testsql.js
+++ blog25/lib/testsql.js
@@ -3,7 +3,8 @@ const mysql = require('mysql2');
 
 // create the connection to database
 const connection = mysql.createConnection({
-  host: 'localhost',
+  host: '192.168.1.141',
+  port:"5432",
   user: 'root',
   password: "123456",
   database: 'test'
@@ -11,8 +12,9 @@ const connection = mysql.createConnection({
 
 // simple query
 connection.query(
-  'SELECT * FROM `table` WHERE `name` = "Page" AND `age` > 45',
+  'show databases;',
   function (err, results, fields) {
+    console.log("err",err)
     console.log(results); // results contains rows returned by server
     console.log(fields); // fields contains extra meta data about results, if available
   }
