5a844ee7e3e59577e3cd905878b6e448bb4abe98
招雄彬 <614619114@qq.com>
2020-10-28 15:39:06 +0800

    fix:测试链接vmware的mysql成功

--- blog25/lib/testsql.js
+++ blog25/lib/testsql.js
@@ -4,10 +4,9 @@ const mysql = require('mysql2');
 // create the connection to database
 const connection = mysql.createConnection({
   host: '192.168.1.141',
-  port:"5432",
-  user: 'root',
-  password: "123456",
-  database: 'test'
+  port:"3306",
+  user:"root",
+  password: "123456"
 });
 
 // simple query
@@ -15,7 +14,7 @@ connection.query(
   'show databases;',
   function (err, results, fields) {
     console.log("err",err)
-    console.log(results); // results contains rows returned by server
-    console.log(fields); // fields contains extra meta data about results, if available
+    console.log("results",results); // results contains rows returned by server
+    console.log("fields",fields); // fields contains extra meta data about results, if available
   }
 );
